6fd4a722efad8b4255dbd8031da455029b3ef34a
EvoArs <EvoyanAG@mail.ru>
2023-06-06 17:25:34 +0300

    Save project

--- src/test/java/TasksTest.java
+++ src/test/java/TasksTest.java
@@ -4,19 +4,68 @@ import org.junit.jupiter.api.Test;
 public class TasksTest {
 
     @Test
-    public void testMethodForTheSearchQueryTrue() {
-        SimpleTask task = new SimpleTask(01, "task");
-        String query = "task";
+    public void testMethodForTheSearchQueryTrueSimpleTask() {
+        SimpleTask task = new SimpleTask(01, "тут есть то самое слово");
+        String query = "то самое слово";
         boolean expected = true;
         boolean actual = task.matches(query);
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    public void testMethodForTheSearchQueryFalse() {
+    public void testMethodForTheSearchQueryFalseSimpleTask() {
         SimpleTask task = new SimpleTask(02, "прыыыыжок");
-        String query = "task";
+        String query = "то самое слово";
         boolean expected = false;
         boolean actual = task.matches(query);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMethodForTheSearchQueryTrueEpic() {
+        String[] subtasks = {"куры", "млеко", "яйки"};
+        Epic epic = new Epic(01, subtasks);
+        String query = "яйки";
+        boolean expected = true;
+        boolean actual = epic.matches(query);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMethodForTheSearchQueryFalseEpic() {
+        String[] subtasks = {"куры", "млеко", "яйки"};
+        Epic epic = new Epic(02, subtasks);
+        String query = "Светлане Соколовой";
+        boolean expected = false;
+        boolean actual = epic.matches(query);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMethodForTheSearchQueryTrueMeeting() {
+        Meeting meeting = new Meeting(
+                01,
+                "Странная музыка 2000-х",
+                "Пушной как средство от бессонницы",
+                "07.06.2023"
+        );
+        String query = "Пушной";
+        boolean expected = true;
+        boolean actual = meeting.matches(query);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMethodForTheSearchQueryFalseMeeting() {
+        Meeting meeting = new Meeting(
+                01,
+                "Странная музыка 2000-х",
+                "Пушной как средство от бессонницы",
+                "07.06.2023"
+        );
+        String query = "2024";
+        boolean expected = false;
+        boolean actual = meeting.matches(query);
         Assertions.assertEquals(expected, actual);
     }
 }
